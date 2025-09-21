f91c8e39701eb1746f2a095049c2fa24a9c9b569
Traube <girlscoutcookies420@gmx.net>
2025-09-21 21:54:44 +0200

    feat: i did day 3 part 2 ⸳̫⸳

--- 2024/Day 3/src/main.rs
+++ 2024/Day 3/src/main.rs
@@ -7,16 +7,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = String::new();
     file.read_to_string(&mut input)?;
 
-    let mut total: u32 = 0;
+    let mut total_p1: u32 = 0;
 
-    let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
-    for captures in regex.captures_iter(&input) {
+    // Part One
+    let regex_p1 = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
+    for captures in regex_p1.captures_iter(&input) {
         let a: u32 = captures[1].parse()?;
         let b: u32 = captures[2].parse()?;
-        total += a * b;
+        total_p1 += a * b;
     }
 
-    println!("Result Part One: {}", total);
+    println!("Result Part One: {}", total_p1);
+
+    // Part Two
+    let regex_p2 = Regex::new(r"(?:mul\((\d{1,3}),(\d{1,3})\))|do\(\)|don't\(\)")?;
+    let mut enabled = true;
+    let mut total_p2: u32 = 0;
+
+    for caps in regex_p2.captures_iter(&input) {
+        if let Some(whole_match) = caps.get(0) {
+            let token = whole_match.as_str();
+            if token.starts_with("mul(") {
+                if enabled {
+                    let a: u32 = caps[1].parse()?;
+                    let b: u32 = caps[2].parse()?;
+                    total_p2 += a * b;
+                }
+            } else if token == "do()" {
+                enabled = true;
+            } else {
+                enabled = false;
+            }
+        }
+    }
+
+    println!("Result Part Two: {}", total_p2);
 
     Ok(())
 }
